564cba724f21b25177d87e6c14710b8e98470087
JoelLeyes <joel.leyes@estudiantes.utec.edu.uy>
2026-05-17 18:13:50 -0300

    COFRE YA NO SE ABRE CON GOLPES y PUERTA arrancada despues continuo con las 2 cosas

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -3,9 +3,6 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class Chest : MonoBehaviour
 {
-    // Tag que deben tener los ataques del jugador (por ejemplo, el prefab de Fire).
-    // Lo dejamos como constante para evitar que Unity lo "guarde" en el Inspector y se desincronice.
-    private const string AttackTag = "Player_Attack";
     private string openTriggerName = "Open";
 
     private Animator animator;
@@ -14,35 +11,6 @@ public class Chest : MonoBehaviour
     private void Awake()
     {
         animator = GetComponent<Animator>();
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (isOpen)
-        {
-            return;
-        }
-
-        // Detecta el ataque aunque el collider esté en un hijo del prefab.
-        string otherTag = other.tag;
-        string rootTag = other.transform.root.tag;
-
-        if (otherTag != AttackTag && rootTag != AttackTag)
-        {
-            return;
-        }
-
-        OpenChest();
-
-        // Destruir solo proyectiles independientes, no hitboxes hijos del Player.
-        if (other.GetComponentInParent<Player>() == null && !other.CompareTag("Object_Destroyable"))
-        {
-            GameObject projectileRoot = other.transform.root.gameObject;
-            if (projectileRoot != gameObject)
-            {
-                Destroy(projectileRoot);
-            }
-        }
     }
 
     private void OpenChest()
